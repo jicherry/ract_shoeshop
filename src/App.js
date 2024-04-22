70bf6a7af34ed816f6c5a194da89351c5de11e9d
chaebeen <tnwl05200@gmail.com>
2024-04-22 23:20:08 +0900

    라우터

--- src/App.js
+++ src/App.js
@@ -2,43 +2,50 @@ import { useState } from 'react';
 import './App.css';
 import { Nav, Container, Navbar } from 'react-bootstrap';
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import Detail from './routes/Detail.js';
+
 
 function App() {
 
   let [shoes] = useState(data)
+  let navigate = useNavigate(); //페이지 이동 도와줌
 
   return (
     <div className="App">
-
-      <Routes>
-        <Route path='/detail' element={<div>상세페이지</div>}/>
-        <Route path='/about' element={<div>어바웃페이지</div>}/>
-      </Routes>
-
        <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">SHOESHOP</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') } }>Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail') }}>Detail</Nav.Link>
           </Nav>
         </Container>
        </Navbar>
 
-       <div className='main-bg'></div>
 
-       <div className='container'>
-        <div className='row'>
-          {
-            shoes.map((a , i) => {
-              return(
-              <Card shoes={shoes[i]} i={i}/>
-              )
-            })
-          }
-        </div>
+       <Routes>
+        <Route path='/' element={ 
+        <div>
+          <div className='main-bg'></div>
+            <div className='container'>
+              <div className='row'>
+              {
+                shoes.map((a , i) => {
+                  return(
+                  <Card shoes={shoes[i]} i={i}/>
+                  )
+                })
+              }
+              </div>
+            </div>
        </div>
+      } />
+      
+        <Route path='/detail/:id' element={<Detail shoes={shoes} />}/> 
+        <Route path='*' element={<div>없는 페이지</div>} />
+      </Routes>
+
     </div>
   );
 }
